abd728b9e38da819de65414b94401f2a55a24f1e
Mark Visschers <visschers.m@gmail.com>
2021-06-17 13:37:02 +0200

    first commit

---
+++ MongoTestBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MatrTech.Utilities.Test.Mongo
+{
+    public class MongoTestBase
+    {
+        private readonly DatabaseManager databaseManager;
+
+        public MongoTestBase()
+        {
+            this.databaseManager = new DatabaseManager();
+        }
+    }
+}
